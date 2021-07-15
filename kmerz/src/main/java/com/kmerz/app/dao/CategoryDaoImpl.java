5b710005ba1a92d218d8b4bfc050674e54ae0e94
yoonsu0411 <82573965+yoonsu0411@users.noreply.github.com>
2021-07-15 10:08:51 +0900

    카테고리 하는중

--- kmerz/src/main/java/com/kmerz/app/dao/CategoryDaoImpl.java
+++ kmerz/src/main/java/com/kmerz/app/dao/CategoryDaoImpl.java
@@ -2,16 +2,24 @@ package com.kmerz.app.dao;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.kmerz.app.vo.CategoryVo;
 
 @Repository
 public class CategoryDaoImpl implements CategoryDao {
+	
+	private static final String NAMESPACE = "com.kmerz.app.category.";
 
+	@Inject
+	SqlSession sqlsession;
+	
 	@Override
 	public List<CategoryVo> selectCategoryList() {
-		// TODO Auto-generated method stub
+		sqlsession.selectList(NAMESPACE + "selectCategoryList");
 		return null;
 	}
 
