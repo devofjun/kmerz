03d7e2aef51a7e0fda50379dc5aa67f9233727b2
잠부족 <beng0516@naver.com>
2021-07-21 23:15:29 +0900

    기존 테이블 수정

--- kmerz/src/main/webapp/resources/sql/kmerz.sql
+++ kmerz/src/main/webapp/resources/sql/kmerz.sql
@@ -1,8 +1,9 @@
 --create user kmerz identified by 1234;
 --grant connect, resource to kmerz;
 
---commit;
+--==================================================
 -- 유저 정보 테이블
+--==================================================
 drop table tbl_member CASCADE CONSTRAINTS;
 --truncate table tbl_member;
 create table tbl_member (
@@ -16,26 +17,15 @@ create table tbl_member (
     constraint unique_user_name unique(user_name)
 );  
 drop sequence seq_user_no;
-create sequence seq_user_no;
+create sequence seq_user_no
+    start with 1000;
 
---select * from tbl_member;
 
--- 유저 로그 테이블
-drop table tbl_member_log CASCADE CONSTRAINTS;
---truncate table tbl_member_log;
-create table tbl_member_log (
-    user_log_code varchar2(20),
-    user_id varchar2(50) references tbl_member(user_id),
-    user_request_ip varchar2(20),
-    user_request_content varchar2(200),
-    user_log_time timestamp default sysdate
-);
-
+--==================================================
 -- 커뮤니티 정보 테이블
+--==================================================
 drop table tbl_community CASCADE CONSTRAINTS;
 --truncate table tbl_community;
-
--- community_id = tag
 create table tbl_community (
     community_id varchar2(30) primary key,
     user_id varchar2(50) references tbl_member(user_id),
@@ -47,69 +37,77 @@ create table tbl_community (
 );
 
 
-select * from tbl_community;
 
+--==================================================
 -- 카테고리 정보 테이블
+--==================================================
 drop table tbl_category CASCADE CONSTRAINTS;
 create table tbl_category(
-    category_id number primary key,
+    category_no number primary key,
     community_id varchar2(30) references tbl_community(community_id),
     category_name varchar2(50),
     category_description varchar2(300),
     category_status varchar2(15)
 );
-drop sequence seq_cate_id;
-create sequence seq_cate_id
-    start with 1
-    minvalue 1;
-
 drop sequence seq_category_id;
-create sequence seq_category_id;
+create sequence seq_category_id
+    start with 100;
 
+
+
+--==================================================
 -- 게시글 테이블
+--==================================================
 drop table tbl_posts CASCADE CONSTRAINTS;
 create table tbl_posts(
     post_no number primary key,
     user_name varchar2(50) references tbl_member(user_name),
     community_id varchar2(50) references tbl_community(community_id),
-    category_id number references tbl_category(category_id),
+    category_no number references tbl_category(category_no),
     post_title varchar2(100),
-    post_content varchar2(500),
+    post_content_file varchar2(500),
+    post_readcount number default 0,
     post_recommand number default 0,
-    post_viewcount number default 0,
     post_lastupdate timestamp default sysdate,
-    post_status varchar2(15),
-    post_media varchar2(10) default 'F'
+    post_status varchar2(15)
 );
-select * from tbl_posts;
 drop sequence seq_post_no;
 create sequence seq_post_no
-    start with 1
-    minvalue 1;
+    start with 100;
 
+
+--==================================================
 -- 댓글 테이블
+--==================================================
 drop table tbl_comment CASCADE CONSTRAINTS;
 create table tbl_comment(
     comment_no number primary key,
     post_no number references tbl_posts(post_no),
-    user_id varchar2(50) references tbl_member(user_id),
+    user_name varchar2(50) references tbl_member(user_name),
     comment_content varchar2(200) not null,
+    comment_reply_count number default 0,
     comment_regist_date timestamp default sysdate
 );
-
 -- 댓글 시퀀스 생성
 drop sequence seq_comment_no;
-create sequence seq_comment_no;
+create sequence seq_comment_no
+    start with 100;
 
+--==================================================
 -- 답글 테이블
+--==================================================
 drop table tbl_reply CASCADE CONSTRAINTS;
 create table tbl_reply(
     reply_no number primary key,
     comment_no number references tbl_comment(comment_no),
-    user_id varchar(50) references tbl_member(user_id),
+    user_name varchar(50) references tbl_member(user_name),
     reply_content varchar2(200) not null,
     reply_regist_date timestamp default sysdate
 );
+drop sequence seq_reply_no;
+create sequence seq_reply_no
+    start  with 100;
+
 
 -- 미디어 테이블
 drop table tbl_media CASCADE CONSTRAINTS;
@@ -120,8 +118,9 @@ create table tbl_media(
     upload_time timestamp default sysdate,
     delete_time timestamp default null
 );
-
-create sequence seq_media_no;
+drop sequence seq_media_no;
+create sequence seq_media_no
+    start with 100;
 
 -- 게시글변경 로그 테이블
 /*
@@ -148,8 +147,9 @@ start with 1;
 
 
 
-
+--==================================================
 -- 관리자 계정 테이블
+--==================================================
 drop table tbl_admin CASCADE CONSTRAINTS;
 create table tbl_admin(
     admin_id varchar(30) primary key,
@@ -163,8 +163,9 @@ insert into tbl_admin
 values
 ('admin', '1234', '관리자');
 
-
+--==================================================
 -- 관리자 메시지 테이블
+--==================================================
 drop table tbl_adminmessage CASCADE CONSTRAINTS;
 create table tbl_adminmessage(
     message_no number primary key,
@@ -172,6 +173,19 @@ create table tbl_adminmessage(
     message_title varchar(100) not null,
     message_content varchar(300),
     message_level number(1) 
+);
+
+--==================================================
+-- 유저 로그 테이블
+--==================================================
+drop table tbl_member_log CASCADE CONSTRAINTS;
+--truncate table tbl_member_log;
+create table tbl_member_log (
+    user_log_code varchar2(20),
+    user_id varchar2(50) references tbl_member(user_id),
+    user_request_ip varchar2(20),
+    user_request_content varchar2(200),
+    user_log_time timestamp default sysdate
 );
 
 commit;
