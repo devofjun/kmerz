70c948e9099d6a507e2fb648f859c5a7e665dd10
yoonsu0411 <82573965+yoonsu0411@users.noreply.github.com>
2021-07-27 21:09:15 +0900

    test

--- kmerz/src/main/java/com/kmerz/app/testController/TestController.java
+++ kmerz/src/main/java/com/kmerz/app/testController/TestController.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kmerz.app.service.CategoryService;
 import com.kmerz.app.service.CommunityService;
+import com.kmerz.app.util.ContentReadAndWrite;
 import com.kmerz.app.vo.CategoryVo;
 
 @Controller
@@ -38,4 +40,12 @@ public class TestController {
 	public String findPassword() {
 		return "member/findPasswordForm";
 	}
+	
+	// 게시물 내용 가져오기
+	@ResponseBody
+	@RequestMapping(value = "/g", method = RequestMethod.GET)
+	public String getPostContent(String post_content_file) throws Exception {
+		return ContentReadAndWrite.ReadContent(post_content_file);
+	}
+	
 }
