5924f48befbd3fe140e767947dc68ac66c0a26a0
zzzless <beng0516@naver.com>
2021-07-29 17:35:41 +0900

    post_status상수 변경하기

--- kmerz/src/main/webapp/resources/sql/testKmerz.sql
+++ kmerz/src/main/webapp/resources/sql/testKmerz.sql
@@ -78,6 +78,9 @@ FOR i IN 1..50 LOOP
 insert into tbl_posts values (seq_post_no.NEXTVAL, 1000, 'star', 100, 'test', 'D:\kmerz\repository\post\2021\7\29\101_d17f37d1-ff26-46b4-b7f4-c5294e6582cc.txt',0,0,sysdate,'accept');
 END LOOP;
 END;
+/
+
+
 --select * from tbl_posts;
 ----------------------------------------------------
 -- 테스트 데이터 삽입(댓글 테이블)
@@ -182,4 +185,5 @@ insert into tbl_banner values(4, 322330);
 -- 테스트 데이터 삽입 끝
 --------------------
 commit;
+
 
