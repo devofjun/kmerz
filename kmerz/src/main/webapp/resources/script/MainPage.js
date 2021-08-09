f9b6617630619d8fd534d05d0bbb0fa121c4a73c
reogamer <park13155@gmail.com>
2021-08-09 10:56:51 +0900

    안짤리게 수정

--- kmerz/src/main/webapp/resources/script/MainPage.js
+++ kmerz/src/main/webapp/resources/script/MainPage.js
@@ -199,33 +199,5 @@ window.onclick = function(event) {
 	  }
 }
 					
-function posttoggle(i) {
-	var postNo = document.getElementById(i);
-	var PostToggleBtn = postNo.querySelector("#post-toggleBtn");
-	var post = postNo.querySelector(".post");
-	var toggleImg = post.querySelector(".rightbar > .postDown");
-	var toggleNavBar = post.querySelectorAll(".leftbar > .BottmNavBar > .toggleNav");
-	if(PostToggleBtn.checked){
-		post.style.height = "180px";
-		post.style.minHeight = "180px";
-		toggleImg.style.transform = "rotateX(0deg)";
-		for(var j = 0; j < toggleNavBar.length; j++){
-		toggleNavBar[j].style.visibility = "hidden";
-		toggleNavBar[j].style.opacity = "0%";
-		toggleNavBar[j].style.height = "0%";
-		toggleNavBar[j].style.transition = "all ease .2s";
-		}
-	}else{
-		post.style.minHeight = "300px";
-		post.style.height = "auto";
-		toggleImg.style.transform = "rotateX(180deg)";
-		for(var j = 0; j < toggleNavBar.length; j++){
-		toggleNavBar[j].style.visibility = "visible";
-		toggleNavBar[j].style.opacity = "100%";
-		toggleNavBar[j].style.height = "100%";
-		toggleNavBar[j].style.transition = "all ease 4s";
-		}	
-	}
-	PostToggleBtn.click();
-}
+
 	
